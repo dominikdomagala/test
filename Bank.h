73baec05214d27a760bf825cc4ae605c06fd28f4
dominik <ddomagala.zst@gmail.com>
2016-11-16 22:39:55 +0100

    EBA-04

--- Bank.h
+++ Bank.h
@@ -1,4 +1,13 @@
 #ifndef _BANK_H
 #define _BANK_H
+#include <stdio.h>
+
+class Bank
+{
+	char *name;
+
+public:
+	Bank(char*);
+}
 
 #endif _BANK_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
+	Bank *bnk;
+	if(argc <= 2)
+		bnk = new Bank(argv[1]);
+	else
+		bnk = new Bank("NBP");
 	return 0;
 }
